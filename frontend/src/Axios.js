b6ebe5bba4d863866f34695c6d9f4310b63e6821
aswinkumar <mynameisaswin321@gmail.com>
2023-03-31 13:55:05 +0530

    updated

--- frontend/src/Axios.js
+++ frontend/src/Axios.js
@@ -1,5 +1,8 @@
 import Axios from 'axios'
-const bu = 'http://localhost:3001/api'
+// for local host
+//const bu = 'http://localhost:3001/api'
+//for production
+const bu = 'http://65.1.94.74/api'
 const axios = Axios.create({
     baseURL:bu,
     withCredentials:true
